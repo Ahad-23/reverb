3aaf63b2e657d436d7d607271b64eac53c0a1627
Ahad-23 <ahad.shaikkh@gmail.com>
2025-05-11 19:32:49 +0530

    Initial commit: frontend + backend boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <Button variant="contained">Hello MUI</Button>
+    </div>
+  );
+}
+
+export default App;
+
